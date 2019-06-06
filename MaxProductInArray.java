cdf361cc5d9ffe019be95f3bf1b0de4ac33ad6b9
karunyashiva <50661970+karunyashiva@users.noreply.github.com>
2019-06-06 20:56:55 +0530

    Update MaxProductInArray.java

--- MaxProductInArray.java
+++ MaxProductInArray.java
@@ -1,3 +1,18 @@
+Maximum product of two numbers
+
+Given an array with all elements greater than or equal to zero. Return the maximum product of two numbers possible.
+
+Note : Save the file / class as “maxProductInArray” with appropriate file extension.
+
+Input:
+The first line of input contains an integer T denoting the number of test cases.
+The first line of each test case is N, size of array. The second line of each test case contains array elements.
+
+Output:
+Print the maximum product of two numbers possible.
+
+program: 
+
 import java.util.*;
 public class MaxProductInArray{
 public static void main(String args[]){
