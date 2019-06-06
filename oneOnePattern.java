62736cdde1340d53e438d08fc51e79949b46937b
karunyashiva <50661970+karunyashiva@users.noreply.github.com>
2019-06-06 21:12:04 +0530

    Date:06.06.2019 Time:8.50

--- oneOnePattern.java
+++ oneOnePattern.java
@@ -1,3 +1,21 @@
+Decode the pattern
+
+Given a pattern as below and an integer n your task is to decode it and print nth row of it. The pattern follows as :
+11
+121
+1331
+14641
+
+Note : Save the file / class as “oneOnePattern” with appropriate file extension.
+
+Input:
+The first line of input is the number of test cases .  Then T test cases follow . The first line of each test case is an integer N.
+
+Output:
+For each test case print the required nth row of the pattern.
+
+program:
+
 import java.util.*;
 public class oneOnePattern{
 public static void main(String args[]){
